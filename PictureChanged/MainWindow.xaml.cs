08c52d493a779e534f054b3cafd79686741bad25
Админ <darpero1@mail.ru>
2021-02-22 14:56:22 +0300

    Добавьте файлы проекта.

---
+++ PictureChanged/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace PictureChanged
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Bitmap> bitmaps;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click_Open(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            fileDialog.DefaultExt = ".jpg";
+            fileDialog.Title = "Please select an image file to encrypt";
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.UriSource = new Uri(fileDialog.FileName);
+                image.EndInit();
+
+                imageCanvas.Source = image;   
+            }
+        }
+
+        private void Slider_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
+        {
+                        
+        }
+            
+    }
+}
